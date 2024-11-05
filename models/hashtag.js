7ca5eb0f909df31f376f34606ed4a9a54f12c8ee
unknown <pidge2@naver.com>
2024-11-05 23:47:06 +0900

    models 추가

---
+++ models/hashtag.js
@@ -0,0 +1,24 @@
+const Sequlize = require('sequelize');
+
+module.exports = class Hashtag extends Sequlize.Model {
+    static init(sequelize) {
+        return super.init({
+            title: {
+                type: Sequlize.STRING(15),
+                allowNull: false,
+                unique: true,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'Hashtag',
+            tableName: 'hashtags',
+            paranoid: false,
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+        });
+    }
+
+    static associate(db) {}
+}
---
+++ models/post.js
@@ -0,0 +1,29 @@
+const Sequlize = require('sequelize');
+
+module.exports = class Post extends Sequlize.Model {
+    static init(sequelize){
+        return super.init({
+            content: {
+                type: Sequlize.STRING(140),
+                allowNull: false,
+            },
+            img: {
+                type: Sequlize.STRING(200),
+                allowNull: true,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'Post',
+            tableName: 'posts',
+            paranoid: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+        });
+    }
+
+    static associate(db) {
+        db.Post.belongsTo(db.User);
+        db.Post.belongsToMany(db.Hastag, { through: 'PostHashtag' });
+    }
+}
